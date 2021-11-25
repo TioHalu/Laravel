ca93a1b513e9ad64953f3dd3a13051e2a9d7b183
TioHalu <you@example.com>
2021-11-25 22:29:30 +0700

    memindahkan method get ke controller dan mendapatkan nilai qeury string

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+class TaskController extends Controller
+{
+    private $tasklist = [
+    'first' => 'sleep',
+    'second' => 'eat',
+    'third' => 'work'
+    ];
+
+    public function index(){
+        if (request()->search){//jika didalam request memilki nilai dari  keyword search
+        return $this->tasklist[request() -> search];
+    }
+    return $this->tasklist;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,48 +29,37 @@ Route::get('Debug',function(){
 });
 
 
-//menggunakan method get dan parameter route
-$tasklist = [
-    'first' => 'sleep',
-    'second' => 'eat',
-    'third' => 'work'
-];
-//method get
-route::get('tasks', function() use($tasklist){
-    return $tasklist;
-});
-//dengan parameter
-route::get('tasks/{params}', function($params) use($tasklist){
-    return $tasklist[$params];
-});
+
+// //method get
+// route::get('tasks', function() use($tasklist){
+//     return $tasklist;
+// });
+// //dengan parameter
+// route::get('tasks/{params}', function($params) use($tasklist){
+//     return $tasklist[$params];
+// });
 
 //mendapatkan data dari query string pada method get
 
-route::get('tasks', function() use($tasklist){
-    // ddd(request());
-    if (request()->search){//jika didalam request memilki nilai dari  keyword search
-        return $tasklist[request() -> search];
-    }
-    return $tasklist;
-});
+route::get('tasks', [TaskController::class,'index']);
 
 //menggunakan method post dan cara mendapatkan datanya akan mengaktifkan crsf protection app/middelcsrfprotect
 
-route::post('/tasks', function() use($tasklist){
-    // return request()->all();
-    $tasklist[request()->label]=request()->task;// seperti atribut name pada input
-    return $tasklist;
-});
+// route::post('/tasks', function() use($tasklist){
+//     // return request()->all();
+//     $tasklist[request()->label]=request()->task;// seperti atribut name pada input
+//     return $tasklist;
+// });
 
-//menggunakan method put dan patch dan mendapatkan data
+// //menggunakan method put dan patch dan mendapatkan data
 
-route::patch('tasks/{key}', function($key) use($tasklist){
-    $tasklist[$key] = request()->task;
-    return $tasklist;
-});
+// route::patch('tasks/{key}', function($key) use($tasklist){
+//     $tasklist[$key] = request()->task;
+//     return $tasklist;
+// });
 
-//menggunakan method delete
-route::delete('tasks{key', function($key) use($tasklist){
-    unset($tasklist[$key]);
-    return $tasklist;
-});
+// //menggunakan method delete
+// route::delete('tasks{key', function($key) use($tasklist){
+//     unset($tasklist[$key]);
+//     return $tasklist;
+// });
