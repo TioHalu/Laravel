478a5c7d0fb3467be8f5f5cff7448abb32651c04
TioHalu <you@example.com>
2021-11-27 22:40:05 +0700

    menampilkan pesan error di masing2 field

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,16 +1,7 @@
 @extends('layouts.App')
 @section('main')
 <div class="mt-5 mx-auto" style="width: 380px">
-<!-- menampilkan pesan error ada di documentasi laravel validation display error -->
-@if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
+
     <div class="card">
         <div class="card-body">
             <!-- action untuk mengirimkan pada halaman task dengan method post -->
@@ -20,10 +11,22 @@
                 <div class="mb-3">
                     <label for="" class="form-label">User</label>
                     <input name="user" type="text" class="form-control">
+                    <!-- menambahkan error dibawah textinput jika inputan kosong -->
+                     @error('user')
+                    <span class="text-danger">
+                        {{$message}}
+                    </span>
+                    @enderror
                 </div>
                 <div class="mb-3">
                     <label for="" class="form-label">Task</label>
                     <textarea name="task" class="form-control" id="" rows="3"></textarea>
+                    <!-- menambahkan error dibawah textinput jika inputan kosong -->
+                     @error('task')
+                    <span class="text-danger">
+                        {{$message}}
+                    </span>
+                    @enderror
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -1,15 +1,7 @@
 @extends('layouts.App')
 @section('main')
 <div class="mt-5 mx-auto" style="width: 380px">
-@if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
+
     <div class="card">
         <div class="card-body">
             <!-- penulisan url harus dengan petik 2 -->
@@ -21,10 +13,22 @@
                 <div class="mb-3">
                     <label for="" class="form-label">User</label>
                     <input name="user" type="text" class="form-control" value="{{ $task->user }}">
+                    <!-- menambahkan error dibawah textinput jika inputan kosong -->
+                    @error('user')
+                    <span class="text-danger">
+                        {{$message}}
+                    </span>
+                    @enderror
                 </div>
                 <div class="mb-3">
                     <label for="" class="form-label">Task</label>
                     <textarea name="task" class="form-control" id="" rows="3" >{{ $task->task }}</textarea>
+                    <!-- menambahkan error dibawah textinput jika inputan kosong -->
+                     @error('task')
+                    <span class="text-danger">
+                        {{$message}}
+                    </span>
+                    @enderror
                 </div>
                 <button type="submit" class="btn btn-primary">Update</button>
             </form>
