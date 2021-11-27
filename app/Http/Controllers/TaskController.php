f302feff221ec39e5995d84b925eeae7d95ffa64
TioHalu <you@example.com>
2021-11-28 01:35:35 +0700

    implementasi middleware role dari data user

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -16,6 +16,7 @@ class TaskController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->middleware('is_admin');
     }
     private $tasklist = [
     'first' => 'sleep',
--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class IsAdmin
 {
@@ -19,7 +20,8 @@ class IsAdmin
     public function handle(Request $request, Closure $next)
     {
         //jika tidak ada data role atau data role tersebut bukan admin maka akan rediret home 
-        if (!$request->role || $request->role != 'admin')
+        $user = Auth::user();//mendapatkan data user nama email dan data role
+        if (!$user->role || $user->role != 'admin')
         {
             return redirect('/');
         }
